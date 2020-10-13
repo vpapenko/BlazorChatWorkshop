f24143b4d5f69a48f924f6f33d73f52450cd04a7
vpapenko <vpapenko@gmail.com>
2020-10-13 14:51:34 +0300

    API is added

---
+++ BlazorChat.API/GCP.deploy.bat
@@ -0,0 +1,2 @@
+dotnet publish
+gcloud app deploy bin\Debug\netcoreapp3.1\publish\app.yaml
---
+++ BlazorChat.API/Services/FirestoreService.cs
@@ -0,0 +1,45 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorChat.API.Services
+{
+    public class FirestoreService
+    {
+        private readonly FirestoreDb _db;
+
+        public FirestoreService()
+        {
+            var projectId = Environment.GetEnvironmentVariable("PROJECT_ID");
+            _db = FirestoreDb.Create(projectId);
+        }
+
+        public async Task<IEnumerable<Dictionary<string, object>>> GetMessages(int maxMessages)
+        {
+            var messages = new List<Dictionary<string, object>>();
+            var messageQuery = _db.Collection("messages").OrderByDescending("date").Limit(maxMessages);
+            var snapshot = await messageQuery.GetSnapshotAsync();
+
+            foreach (var document in snapshot.Documents)
+            {
+                var documentDictionary = document.ToDictionary();
+                documentDictionary["date"] = ((Timestamp)documentDictionary["date"]).ToDateTime();
+                messages.Add(documentDictionary);
+            }
+            return messages;
+        }
+
+        public async Task SaveMessage(string text, string name, DateTime date)
+        {
+            var docRef = _db.Collection("messages").Document();
+            var messageDict = new Dictionary<string, object>
+            {
+                { "text", text },
+                { "name", name },
+                { "date", Timestamp.FromDateTime(date) }
+            };
+            await docRef.SetAsync(messageDict);
+        }
+    }
+}
