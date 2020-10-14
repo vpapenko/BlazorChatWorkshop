4eabd29000ef63408b5d237637e4e2bd64b31d79
vpapenko <vpapenko@gmail.com>
2020-10-14 11:55:57 +0300

    Client is added

---
+++ BlazorChat.Client/GCP.deploy.bat
@@ -0,0 +1,2 @@
+dotnet publish
+gcloud app deploy bin\Debug\netstandard2.1\publish\app.yaml
---
+++ BlazorChat.Client/ViewModels/MessageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BlasorChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat.ViewModels
+{
+    public class MessageViewModel
+    {
+        public string Text { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public static implicit operator MessageViewModel(Message message)
+        {
+            return new MessageViewModel()
+            {
+                Text = message.Text,
+                Name = message.Name,
+                Date = message.Date
+            };
+        }
+    }
+}
