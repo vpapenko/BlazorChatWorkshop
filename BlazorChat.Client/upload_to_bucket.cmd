7436e7a69d60830c956f692ce59f4aa3bc065c80
vpapenko <vpapenko@gmail.com>
2020-10-17 22:19:11 +0300

    client is removed

--- BlazorChat.Client/upload_to_bucket.cmd
+++
@@ -1,5 +0,0 @@
-﻿dotnet restore
-dotnet publish -c Release
-gsutil rsync -R bin\Release\netstandard2.1\publish\wwwroot gs://blazor-chat-client
-gsutil web set -m index.html -e index.html gs://blazor-chat-client
-gsutil setmeta -r -h "Cache-control:no-cache" gs://blazor-chat-client
