4f7caf289d0b0bf4986a792bd67d6ba935d683d1
vpapenko <vpapenko@gmail.com>
2020-10-26 14:13:39 +0300

    SignalR is added

--- BlazorChat.Client/Program.cs
+++ BlazorChat.Client/Program.cs
@@ -17,11 +17,13 @@ namespace BlazorChat.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddScoped(sp => new HttpClient());
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<UserViewModel>();
-            builder.Services.AddScoped<MessageViewModel>();
-
-            await builder.Build().RunAsync();
+            builder.Services.AddSingleton<MessageViewModel>();
+            var host = builder.Build();
+            MessageViewModel messageViewModel = host.Services.GetRequiredService<MessageViewModel>();
+            await messageViewModel.Init();
+            await host.RunAsync();
         }
     }
 }
--- BlazorChat.Client/ViewModels/MessageViewModel.cs
+++ BlazorChat.Client/ViewModels/MessageViewModel.cs
@@ -1,33 +1,59 @@
 ﻿using BlasorChat.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace BlazorChat.ViewModels
 {
-    public class MessageViewModel
+    public class MessageViewModel : INotifyPropertyChanged
     {
-        public MessageViewModel(HttpClient httpClient)
+        public MessageViewModel(HttpClient httpClient, UserViewModel userViewModel)
         {
             _httpClient = httpClient;
+            _userViewModel = userViewModel;
         }
 
+        private HubConnection _hubConnection;
         private readonly HttpClient _httpClient;
-        public IEnumerable<Message> Messages;
+        private readonly UserViewModel _userViewModel;
 
+        public List<Message> Messages = new List<Message>();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string NewMessage { get; set; }
+        public bool MessageCanBeSend => !string.IsNullOrEmpty(NewMessage) && !string.IsNullOrEmpty(_userViewModel.Name);
 
-        public async Task LoadMessages()
+        public async Task Init()
         {
-            Messages = await _httpClient.GetFromJsonAsync<Message[]>("https://blazor-chat.ew.r.appspot.com/api/chat");
+            var messages = await _httpClient.GetFromJsonAsync<Message[]>("https://localhost:5001/api/chat");
+            Messages.Clear();
+            Messages.AddRange(messages);
+            _hubConnection = new HubConnectionBuilder().WithUrl("https://localhost:5001/api/chathub").Build();
+
+            _hubConnection.On<Message>("ReceiveMessage", (message) =>
+            {
+                Messages.Add(message);
+                OnPropertyChanged();
+            });
+
+            await _hubConnection.StartAsync();
         }
 
         public async Task SendMessage(string message, string name)
         {
-            await _httpClient.PutAsJsonAsync<Message>("https://blazor-chat.ew.r.appspot.com/api/chat", new Message() { Text = message, Name = name });
-            await LoadMessages();
+            await _hubConnection.SendAsync("SendMessage", new Message() { Text = message, Name = name });
+            NewMessage = "";
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
